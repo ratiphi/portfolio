bd23084519706023f6a82aff1e09d139121e680f
Tim Turner <timmyturner@me.com>
2017-06-19 11:51:01 -0700

    saving work to layout and hamburger menu

---
+++ js/hamburger.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// $(document).ready(function() {
+//   $('.menu-btn').on('click', function() {
+//     $('.responsive-menu').addClass('expand');
+//     $('.menu-btn').addClass('btn-none');
+//   });
+//   $('.close-btn').on('click', function() {
+//     $('.responsive-menu').removeClass('expand');
+//     $('.menu-btn').removeClass('btn-none');
+//   });
+// });
+
+$(document).ready(function() {
+  $('.menu-btn').on('click', function() {
+    $('.responsive-menu').toggleClass('expand');
+  });
+});
